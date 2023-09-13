4a8cc59c432c316a631581ad8ef9ef87230fafe9
Sebástian Narváez <sebnarvaez19@gmail.com>
2023-09-13 15:18:38 +0000

    feat: add functionality to the package

--- src/straticor/__init__.py
+++ src/straticor/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("straticor")
+
+
+from .utils import load_column
+from .plot import plot_warpingpaths, plot_column
+from .dtw import dtw_log, warp_log
--- src/straticor/straticor.py
+++
@@ -1,3 +0,0 @@
-from .utils import load_column
-from .plot import plot_warpingpaths, plot_column
-from .dtw import dtw_log, warp_log
